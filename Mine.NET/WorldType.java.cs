d821c6de7abfbfd0e4506f9a3d1229c54727da4e
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-05-11 20:43:08 +0200

    I am Spartacus

--- Mine.NET/WorldType.java.cs
+++ Mine.NET/WorldType.java.cs
@@ -1,48 +1,27 @@
-package org.bukkit;
 
-import com.google.common.collect.Maps;
-import java.util.Map;
-
+using System;
+using System.Collections.Generic;
 /**
- * Represents various types of worlds that may exist
- */
-public enum WorldType {
-    NORMAL("DEFAULT"),
-    FLAT("FLAT"),
-    VERSION_1_1("DEFAULT_1_1"),
-    LARGE_BIOMES("LARGEBIOMES"),
-    AMPLIFIED("AMPLIFIED"),
-    CUSTOMIZED("CUSTOMIZED");
-
-    private readonly static Dictionary<String, WorldType> BY_NAME = Maps.newHashMap();
-    private readonly String name;
-
-    private WorldType(String name) {
-        this.name = name;
+* Represents various types of worlds that may exist
+*/
+public static class WorldType {
+    public enum WorldTypes
+    {
+        NORMAL,
+        FLAT,
+        VERSION_1_1,
+        LARGE_BIOMES,
+        AMPLIFIED,
+        CUSTOMIZED
     }
 
-    /**
-     * Gets the name of this WorldType
-     *
-     * @return Name of this type
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Gets a Worldtype by its name
-     *
-     * @param name Name of the WorldType to get
-     * @return Requested WorldType, or null if not found
-     */
-    public static WorldType getByName(String name) {
-        return BY_NAME.get(name.toUpperCase());
-    }
-
-    static {
-        for (WorldType type : values()) {
-            BY_NAME.put(type.name, type);
-        }
-    }
+    private readonly static Dictionary<WorldTypes, string> BY_NAME = new Dictionary<WorldTypes, string>()
+    {
+        { WorldTypes.NORMAL, "DEFAULT" },
+        { WorldTypes.FLAT, "FLAT" },
+        { WorldTypes.VERSION_1_1, "DEFAULT_1_1" },
+        { WorldTypes.LARGE_BIOMES, "LARGEBIOMES" },
+        { WorldTypes.AMPLIFIED, "AMPLIFIED" },
+        { WorldTypes.CUSTOMIZED, "CUSTOMIZED" }
+    };
 }
